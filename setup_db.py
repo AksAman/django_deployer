21806dbf2b33b7b7209234f2a038af6b0dd57df9
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-12-23 12:05:05 +0530

    update env load line

--- setup_db.py
+++ setup_db.py
@@ -191,14 +191,13 @@ def main(
         logger.error(f"No .env file found at {str(env_file_path)}")
         return
 
-    db_name = os.environ.get("DB_NAME")
-    db_user = os.environ.get("DB_USER")
-    db_password = os.environ.get("DB_PASSWORD")
-    db_host = os.environ.get("DB_HOST")
-    db_port = os.environ.get("DB_PORT")
-
     try:
         load_dotenv_from_file(env_file_path)
+        db_name = os.environ.get("DB_NAME")
+        db_user = os.environ.get("DB_USER")
+        db_password = os.environ.get("DB_PASSWORD")
+        db_host = os.environ.get("DB_HOST")
+        db_port = os.environ.get("DB_PORT")
 
         create_postgres_resources(artifacts_dir, db_name, db_user, db_password, db_host, db_port, execute_sql)
         if migrate:
