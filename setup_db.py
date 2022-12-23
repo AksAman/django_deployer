56d9c30c5e5b879576e2e3366dd44e48b5a872e5
AksAman@personal_pc <krsingh.aman@gmail.com>
2022-12-23 11:59:43 +0530

    add try except

--- setup_db.py
+++ setup_db.py
@@ -185,19 +185,27 @@ def main(
         logger.error(f"No .env file found at {str(env_file_path)}")
         return
 
-    load_dotenv_from_file(env_file_path)
-
     db_name = os.environ.get("DB_NAME")
     db_user = os.environ.get("DB_USER")
     db_password = os.environ.get("DB_PASSWORD")
     db_host = os.environ.get("DB_HOST")
     db_port = os.environ.get("DB_PORT")
 
-    create_postgres_resources(artifacts_dir, db_name, db_user, db_password, db_host, db_port, execute_sql)
-    if migrate:
-        if "VIRTUAL_ENV" not in os.environ:
-            raise DeploymentException("Virtualenv not activated, please activate it first")
-        migrate_db(project_dir)
+    try:
+        load_dotenv_from_file(env_file_path)
+
+        create_postgres_resources(artifacts_dir, db_name, db_user, db_password, db_host, db_port, execute_sql)
+        if migrate:
+            if "VIRTUAL_ENV" not in os.environ:
+                raise DeploymentException("Virtualenv not activated, please activate it first")
+            migrate_db(project_dir)
+
+        restart_services()
+    except DeploymentException as e:
+        logger.error(e)
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 if __name__ == "__main__":
